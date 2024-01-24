964c89f0d6c0fea27efed33d555f00cb60afdf87
DanielleScott <dmyers28@cnm.edu>
2024-01-24 12:01:27 -0700

    Testing in progress.

--- src/main/java/edu/cnm/deepdive/shuffler/Shuffler.java
+++ src/main/java/edu/cnm/deepdive/shuffler/Shuffler.java
@@ -19,4 +19,77 @@ public class Shuffler {
       }
     }
 
+  public void shuffle(long[] data) {
+    for (int destIndex = data.length - 1; destIndex > 0; destIndex--) {
+      int srcIndex = rng.nextInt(destIndex + 1);
+      long temp =  data[destIndex];
+      data[destIndex] = data[srcIndex];
+      data[srcIndex] = temp;
+    }
+  }
+
+  public void shuffle(boolean[] data) {
+    for (int destIndex = data.length - 1; destIndex > 0; destIndex--) {
+      int srcIndex = rng.nextInt(destIndex + 1);
+      boolean temp =  data[destIndex];
+      data[destIndex] = data[srcIndex];
+      data[srcIndex] = temp;
+    }
+  }
+
+  public void shuffle(char[] data) {
+    for (int destIndex = data.length - 1; destIndex > 0; destIndex--) {
+      int srcIndex = rng.nextInt(destIndex + 1);
+      char temp =  data[destIndex];
+      data[destIndex] = data[srcIndex];
+      data[srcIndex] = temp;
+    }
+  }
+
+  public void shuffle(short[] data) {
+    for (int destIndex = data.length - 1; destIndex > 0; destIndex--) {
+      int srcIndex = rng.nextInt(destIndex + 1);
+      short temp =  data[destIndex];
+      data[destIndex] = data[srcIndex];
+      data[srcIndex] = temp;
+    }
+  }
+
+  public void shuffle(float[] data) {
+    for (int destIndex = data.length - 1; destIndex > 0; destIndex--) {
+      int srcIndex = rng.nextInt(destIndex + 1);
+      float temp =  data[destIndex];
+      data[destIndex] = data[srcIndex];
+      data[srcIndex] = temp;
+    }
+  }
+
+  public void shuffle(double[] data) {
+    for (int destIndex = data.length - 1; destIndex > 0; destIndex--) {
+      int srcIndex = rng.nextInt(destIndex + 1);
+      double temp =  data[destIndex];
+      data[destIndex] = data[srcIndex];
+      data[srcIndex] = temp;
+    }
+  }
+
+  public void shuffle(byte[] data) {
+    for (int destIndex = data.length - 1; destIndex > 0; destIndex--) {
+      int srcIndex = rng.nextInt(destIndex + 1);
+      byte temp =  data[destIndex];
+      data[destIndex] = data[srcIndex];
+      data[srcIndex] = temp;
+    }
+  }
+
+  public void shuffle(Object[] data) {
+    for (int destIndex = data.length - 1; destIndex > 0; destIndex--) {
+      int srcIndex = rng.nextInt(destIndex + 1);
+      Object temp =  data[destIndex];
+      data[destIndex] = data[srcIndex];
+      data[srcIndex] = temp;
+    }
+  }
+
+
 }
---
+++ src/test/java/edu/cnm/deepdive/shuffler/ShufflerTest.java
@@ -0,0 +1,21 @@
+package edu.cnm.deepdive.shuffler;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import org.junit.jupiter.api.Test;
+
+class ShufflerTest {
+
+  @Test
+  void shuffle_ints() {
+    int[] data = IntStream.range(0, 1_000_000)
+        .toArray();
+    Set<Integer> check = IntStream.of(data)
+        .boxed()
+        .collect(Collectors.toSet());
+  }
+
+}
